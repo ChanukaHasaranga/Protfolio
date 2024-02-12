5486214592f15d1016ae7f09dfab474e89c77429
Chanuka Hasaranga <96354974+ChanukaHasaranga@users.noreply.github.com>
2024-02-12 18:14:05 +0530

    changes

--- lib/changes/links.dart
+++ lib/changes/links.dart
@@ -1,20 +1,16 @@
 // CV
 const String resume =
-    'https://drive.google.com/file/d/1bDNNrkQtNVJbXcdwMSx0crQQvAF_nMwV/view?usp=sharing';
+    'https://drive.google.com/file/d/12yeBeuT-emVoIwwVGE0Y7ixgoESpF0Fv/view?usp=sharing';
 
 // Github Link
-const String gitHub = 'https://github.com/sudeshnb';
+const String gitHub = 'https://github.com/ChanukaHasaranga';
 // Github Link
 const String facebook = 'https://facebook.com/sudeshnb';
 // Github Link
-const String twitter = 'https://twitter.com/sudesh78';
 // Github Link
-const String youtube =
-    'https://www.youtube.com/channel/UCXooUY2oL_eqGhTaZn-ExSg';
 // Github Link
-const String linkedin = 'https://linkedin.com/in/sudeshnb';
+const String linkedin = 'https://www.linkedin.com/in/chanuka-hasaranga-207b62218/';
 // Github Link
-const String buymeacoffee = 'https://www.buymeacoffee.com/sudeshnb';
 // Github Link
 const String instagram = 'https://instagram.com/sudesh_nish';
 // socialLinks
